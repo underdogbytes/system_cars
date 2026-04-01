3b93ebe20cffafbe0ae3ebbac9188336344ac83d
bea <beatriztattooer@gmail.com>
2026-04-01 00:10:33 -0300

    :recycle: refactor[routes]: padronização nome rotas

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const CarController = require('../controllers/CarController');
 
 router.get('/', CarController.getAll);
-router.get('/id=:id', CarController.getById);
-router.get('/plate=:plate', CarController.getByPlate);
+router.get('/:id', CarController.getById);
+router.get('/plate/:plate', CarController.getByPlate);
 router.post('', CarController.createCar);
 router.put('/:id', CarController.editCar);
 router.delete('/:id', CarController.deleteCar);
