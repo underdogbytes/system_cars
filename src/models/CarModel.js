1ce69c9c4d1e60b705c6053254a9ccc3774f6df1
bea <beatriztattooer@gmail.com>
2026-04-09 17:22:52 -0300

    :construction: wip[model]: getters & setters

--- src/models/CarModel.js
+++ src/models/CarModel.js
@@ -20,6 +20,18 @@ class CarModel {
       color: this.#color
     };
   }
+
+  // Getters
+  get id() { return this.#id; }
+  get model() { return this.#model; }
+  get plate() { return this.#plate; }
+  get color() { return this.#color; }
+
+  // Setters
+  set id(value) { return this.#id = value; }
+  set model(value) { return this.#model = value; }
+  set plate(value) { return this.#plate = value; }
+  set color(value) { return this.#color = value; }
 }
 
 module.exports = CarModel;
