61d395cc7aef1d32c75a1f749cf5a2eb8f496715
bea <beatriztattooer@gmail.com>
2026-04-08 22:34:36 -0300

    :bug: fix: carModel
    Dados do objeto não estavam aparecendo em razão das props privadas

--- src/models/CarModel.js
+++ src/models/CarModel.js
@@ -1,11 +1,24 @@
 const db = require('../db');
 
 class CarModel {
+  #id;
+  #model;
+  #plate;
+  #color;
   constructor(id, model, plate, color) {
-    this.id = id;
-    this.model = model;
-    this.plate = plate;
-    this.color = color;
+    this.#id = id;
+    this.#model = model;
+    this.#plate = plate;
+    this.#color = color;
+  }
+
+  toJSON() {
+    return {
+      id: this.#id,
+      model: this.#model,
+      plate: this.#plate,
+      color: this.#color
+    };
   }
 }
 
